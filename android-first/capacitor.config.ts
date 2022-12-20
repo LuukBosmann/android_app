db6ec774f2d120a2c910495a9138fd7084abbae8
Luuk <luuk.bosmann@student.graafschapcollege.nl>
2022-12-20 10:08:25 +0100

    added ionic

---
+++ android-first/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'android-first',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
